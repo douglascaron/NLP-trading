6c31b225811dc9566452ae0b3d2a1c22cab58c7b
douglascaron <rsc.douglas@gmail.com>
2023-12-13 11:52:34 +0000

    Delete unnecessary files and update code

--- data/sentiment_calibration.py
+++
@@ -1,8 +0,0 @@
-from nltk.sentiment import SentimentIntensityAnalyzer
-
-while True: 
-    data = input("News: ")
-
-    sia = SentimentIntensityAnalyzer()
-    sentiment_score = sia.polarity_scores(data)['compound']
-    print(sentiment_score)
